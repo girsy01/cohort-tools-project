cb3519c664f093482bc259ce648667ad379b9071
Franziska Scheer <franzi-scheer@web.de>
2025-01-16 21:37:30 +0100

    small bug fix

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,19 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 function isAuthenticated(req, res, next) {
-    console.log("in the authentication middleware function");
+  console.log("in the authentication middleware:", req.headers.authorization);
 
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.split(" ")[0] === "Bearer"
-    ) {
-        const theToken = req.headers.authorizations.split(" ")[1];
-        const payload = jwt.verify(theToken, process.env.TOKEN_SECRET);
+  if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+    const theToken = req.headers.authorization.split(" ")[1];
+    const payload = jwt.verify(theToken, process.env.TOKEN_SECRET);
 
-        req.payload = payload;
-        next();
-    } else {
-        res.status(403).json({ message: "no token present" });
-    }
+    req.payload = payload;
+    next();
+  } else {
+    res.status(403).json({ message: "no token present" });
+  }
 }
 module.exports = { isAuthenticated };
